be171240e4c9ff64ea336dc1a14b1584855399cf
Clara-Manzano <clara.manzano.corona@gmail.com>
2024-04-03 09:40:53 +0200

    Usuario guardado en variable

--- src/utils/variables.js
+++ src/utils/variables.js
@@ -1,3 +1,4 @@
 export const API_URL = 'https://proyecto-10-backend.vercel.app/api/v1'
 const token = localStorage.getItem('token')
-export const User = !!token
+const usuarioData = localStorage.getItem('user', JSON.stringify())
+export const User = !!token && usuarioData
