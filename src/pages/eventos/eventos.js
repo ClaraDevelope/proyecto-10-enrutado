2645f077fb2b3c5d91e87e261aa6b083dfdea00d
Clara-Manzano <clara.manzano.corona@gmail.com>
2024-04-17 12:52:05 +0200

    crear eventos y eliminarlos check!

--- src/pages/eventos/eventos.js
+++ src/pages/eventos/eventos.js
@@ -58,6 +58,7 @@ const mostrarEvento = async (eventoId, elementoPadre) => {
     <div class='button-container'>
         <button class='asistencia' id='ver-asistentes'>Ver asistentes</button>
         <button class='info-boton' id='editar-evento'>Editar evento</button>
+        <button class='eliminar' id='eliminar-evento'>Eliminar</button>
     </div>
 `
     const buttonAsistentes = info.querySelector('#ver-asistentes')
@@ -69,12 +70,33 @@ const mostrarEvento = async (eventoId, elementoPadre) => {
       formEditarEventos(eventoId)
     })
 
+    const eliminarButton = info.querySelector('#eliminar-evento')
+    eliminarButton.addEventListener('click', () => {
+      eliminarEvento(eventoId)
+    })
     elementoPadre.append(titulo, divCartel, info)
   } catch (error) {
     console.error('Error al mostrar evento:', error)
   }
 }
-
+const eliminarEvento = async (eventoId) => {
+  const opciones = {
+    method: 'DELETE',
+    headers: {
+      Authorization: `Bearer ${localStorage.getItem('token')}`
+    }
+  }
+  try {
+    const response = await fetch(API_URL + `/eventos/${eventoId}`, opciones)
+    if (response.ok) {
+      console.log('evento eliminado correctamente')
+    } else {
+      console.log('no se ha podiddo eliminar el evento')
+    }
+  } catch (error) {
+    console.log('error al eliminar el evento', error.message)
+  }
+}
 const verAsistentes = async (evento) => {
   const main = document.querySelector('main')
   main.innerHTML = ''
@@ -107,43 +129,39 @@ const formEditarEventos = (eventoId) => {
   const main = document.querySelector('main')
   main.innerHTML = ''
 
-  const userId = datosUsuario._id
-
   const formulario = document.createElement('form')
   formulario.id = 'form-evento'
   formulario.innerHTML = ` 
   <h2 class='title-edit'>Edita tu evento</h2>
-<label class='start' for="titulo">Título del evento:</label>
-<input id='titulo' type="text" name="titulo">
-<label class='start' for="fecha">Fecha:</label>
-<input id='fecha' type="date" class="date" name="fecha">
-<label class='start'>Ubicación:</label>
-<input id= 'ubicacion' type="text" class="ubicacion" name="ubicación">
-<label class='start' for="descripcion">Descripción:</label>
-<input id='descripcion' type="text" name="descripcion">
-<label class='start' for="precio">Precio:</label>
-<input id='precio' type="number" name="precio">
-<label class='start' for="cartel">Cartel:</label>
-<input id='cartel' type="file" name="cartel" accept="image/*">
-<button class='submit' id='editar-button'>Editar</button>
+  <label class='start' for="titulo">Título del evento:</label>
+  <input id='titulo' type="text" name="titulo">
+  <label class='start' for="fecha">Fecha:</label>
+  <input id='fecha' type="date" class="date" name="fecha">
+  <label class='start'>Ubicación:</label>
+  <input id= 'ubicacion' type="text" class="ubicacion" name="ubicación">
+  <label class='start' for="descripcion">Descripción:</label>
+  <input id='descripcion' type="text" name="descripcion">
+  <label class='start' for="precio">Precio:</label>
+  <input id='precio' type="number" name="precio">
+  <label class='start' for="cartel">Cartel:</label>
+  <input id='cartel' type="file" name="cartel" accept="image/*">
+  <button class='submit' id='editar-button'>Editar</button>
 `
   formulario.addEventListener('submit', async (event) => {
     event.preventDefault()
-    const formData = new FormData()
-    formData.append('titulo', document.getElementById('titulo').value)
-    formData.append('fecha', document.getElementById('fecha').value)
-    formData.append('ubicacion', document.getElementById('ubicacion').value)
-    formData.append('descripcion', document.getElementById('descripcion').value)
-    formData.append('precio', document.getElementById('precio').value)
-    const cartelInput = document.getElementById('cartel')
-    if (cartelInput.files.length > 0) {
-      formData.append('cartel', cartelInput.files[0])
-    }
-    try {
-      await editarEvento(eventoId, userId, formData)
-    } catch (error) {
-      console.error('Error al editar el evento:', error)
-    }
+    const formData = new FormData(formulario)
+    const userId = datosUsuario._id
+    // formData.append('titulo', document.getElementById('titulo').value)
+    // formData.append('fecha', document.getElementById('fecha').value)
+    // formData.append('ubicacion', document.getElementById('ubicacion').value)
+    // formData.append('descripcion', document.getElementById('descripcion').value)
+    // formData.append('precio', document.getElementById('precio').value)
+    // const cartelInput = document.getElementById('cartel')
+    // if (cartelInput.files.length > 0) {
+    //   formData.append('cartel', cartelInput.files[0])
+    // }
+
+    await editarEvento(eventoId, userId, formData)
   })
 
   main.append(formulario)
@@ -154,41 +172,40 @@ const editarEvento = async (eventoId, userId, formData) => {
     method: 'PATCH',
     body: formData,
     headers: {
-      'Content-Type': 'multipart/form-data',
+      // 'Content-type': 'multipart/form-data',
       Authorization: `Bearer ${localStorage.getItem('token')}`
     }
   }
-  console.log(formData)
   for (const [key, value] of formData.entries()) {
     console.log(key + ': ' + value)
   }
+
   try {
     const response = await fetch(
       API_URL + `/eventos/${eventoId}/auth/${userId}`,
       opciones
     )
-
-    const respuestaFinal = await response.json()
-
+    // const data = await response.json()
     console.log(response)
-    if (!response.ok) {
-      console.log('error al editar el evento', response.status)
+    if (response.status === 200) {
+      console.log('Evento editado exitosamente')
     } else {
-      console.log('Evento editado exitosamente:', respuestaFinal)
+      console.error('Error al editar el evento:', error.message)
     }
   } catch (error) {
-    console.error('Error al editar el evento:', error)
+    console.error('Error al editar el evento:', error.message)
   }
 }
 
 const formCrearEvento = () => {
   const main = document.querySelector('main')
+  main.innerHTML = ''
 
   const form = document.createElement('form')
   form.id = 'eventoForm'
 
   form.innerHTML = `
-      <h2>Crea un nuevo evento</h2>
+      <h2 class='title-edit'>Crea un nuevo evento</h2>
       <label for="titulo">Título:</label>
       <input type="text" id="titulo" name="titulo" required><br>
       
@@ -207,10 +224,49 @@ const formCrearEvento = () => {
       <label for="cartel">Cartel:</label>
       <input type="file" id="cartel" name="cartel" accept="image/*" required><br>
       
-      <button type="submit">Enviar</button>
+      <button class='submit' type="submit">Enviar</button>
   `
 
-  // form.addEventListener('submit', enviarFormulario);
+  form.addEventListener('submit', async (e) => {
+    e.preventDefault()
+    const userId = datosUsuario._id
+    const formData = new FormData(form)
+    await enviarFormulario(userId, formData, form)
+  })
 
   main.appendChild(form)
+}
+
+const enviarFormulario = async (userId, formData, form) => {
+  const opciones = {
+    method: 'POST',
+    body: formData,
+    headers: {
+      Authorization: `Bearer ${localStorage.getItem('token')}`
+    }
+  }
+
+  for (const [key, value] of formData.entries()) {
+    console.log(key + ': ' + value)
+  }
+
+  try {
+    const response = await fetch(API_URL + `/auth/${userId}/create`, opciones)
+    if (response.status === 201) {
+      const data = await response.json()
+      console.log('Datos de la respuesta:', data)
+    } else {
+      console.error('Error en la solicitud:', response.status)
+      const errorMessage = await response.text()
+      console.error('Mensaje de error:', errorMessage)
+    }
+  } catch (error) {
+    console.error('Error en la solicitud:', error)
+    const pError = document.createElement('p')
+    pError.classList.add('error')
+    pError.textContent = error.message || 'Error al registrar el evento'
+    pError.style.color = 'blue'
+    pError.style.fontSize = '20px'
+    form.append(pError)
+  }
 }
